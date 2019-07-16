4c02be0ea666f0ace94b17c3c1345d527a61cfa8
Liam Duffy <liamdanielduffy@gmail.com>
2019-07-16 22:22:40 +0530

    attempt to use singleton

--- libraries/apollo/getContext.js
+++ libraries/apollo/getContext.js
@@ -1,14 +1,7 @@
-import initializeAdminApp from "libraries/firebase/initializeAdminApp";
-
-//this throws an error on recompilation
-const firebase = initializeAdminApp();    
+import app from "libraries/firebase/initializeAdminApp";   
 
 export default async () => {  
-
-    //this doesn't throw an error on recompilation
-    // const firebase = initializeAdminApp();    
-
     return {
-        firebase
+        app
     }
 } 
--- libraries/firebase/initializeAdminApp.js
+++ libraries/firebase/initializeAdminApp.js
@@ -2,13 +2,12 @@ import getKey from "./getKey";
 const admin = require("firebase-admin");
 import uuidv4 from 'uuid/v4'
 
-export default () => {
-    const key = getKey();
-    const name = uuidv4()
-	let app = admin.initializeApp({
-        credential: admin.credential.cert(key),
-        databaseURL:"https://fir-bug-repro.firebaseio.com",
-        name
-	});
-	return app;
-};
+const key = getKey();
+const name = uuidv4();
+let app = admin.initializeApp({
+    credential: admin.credential.cert(key),
+    databaseURL: "https://fir-bug-repro.firebaseio.com",
+    name
+});
+
+export default app;
