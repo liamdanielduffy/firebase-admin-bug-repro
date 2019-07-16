c6fa558ece2274219fbb268f29e0e61cfcd70798
Liam Duffy <liamdanielduffy@gmail.com>
2019-07-16 22:16:18 +0530

    error vs no error on recompilation exmaple

--- libraries/apollo/getContext.js
+++ libraries/apollo/getContext.js
@@ -1,8 +1,13 @@
 import initializeAdminApp from "libraries/firebase/initializeAdminApp";
 
+//this throws an error on recompilation
 const firebase = initializeAdminApp();    
 
 export default async () => {  
+
+    //this doesn't throw an error on recompilation
+    // const firebase = initializeAdminApp();    
+
     return {
         firebase
     }
