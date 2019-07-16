ca589c546d02c44dcd341cb7cd6e425924c61b1f
Liam Duffy <liamdanielduffy@gmail.com>
2019-07-16 21:50:15 +0530

    adds lines logging type and contents of service account key

--- libraries/apollo/getContext.js
+++ libraries/apollo/getContext.js
@@ -3,6 +3,7 @@ import initializeAdminApp from "libraries/firebase/initializeAdminApp";
 const firebase = initializeAdminApp();
 
 export default async () => {
+    
     return {
         firebase
     }
--- libraries/firebase/initializeAdminApp.js
+++ libraries/firebase/initializeAdminApp.js
@@ -2,7 +2,9 @@ import { initializeApp, credential } from "firebase-admin";
 import getKey from "./getKey";
 
 export default () => {
-	const key = getKey();
+    const key = getKey();
+    console.log(key)
+    console.log(typeof key)
 	app = initializeApp({
 		credential: credential.cert(key)
 	});
