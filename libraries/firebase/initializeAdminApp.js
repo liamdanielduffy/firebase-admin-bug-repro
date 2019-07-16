d90d7fdf7239f84799d9ea4bc28f4c7d17557b03
Liam Duffy <liamdanielduffy@gmail.com>
2019-07-16 23:33:44 +0530

    removes uuidv4 and app naming, adds check against admin.apps.length to avoid multiple initalizations while developing locall

--- libraries/firebase/initializeAdminApp.js
+++ libraries/firebase/initializeAdminApp.js
@@ -1,17 +1,13 @@
 import getKey from "./getKey";
 const admin = require("firebase-admin");
-import uuidv4 from 'uuid/v4'
 
 const key = getKey();
-const name = uuidv4();
-console.log('\n\n\n\n')
-console.log(admin.apps.length)
-console.log(name)
-console.log("\n\n\n\n");
-let app = admin.initializeApp({
-    credential: admin.credential.cert(key),
-    databaseURL: "https://fir-bug-repro.firebaseio.com",
-    name
+let app
+if(admin.apps.length === 0) {
+ app = admin.initializeApp({
+	credential: admin.credential.cert(key),
+	databaseURL: "https://fir-bug-repro.firebaseio.com"
 });
+}
 
 export default app;
